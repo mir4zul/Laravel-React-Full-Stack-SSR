d8ae15ada17c071b71e37c82915bcc8670cd8b00
miraz66 <mirajulislam0509@gmail.com>
2025-06-28 00:07:51 +0600

    Start ssr project

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <>
+            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
+                <h1 className="bg-amber-300 px-4">hello world</h1>
+            </div>
+        </>
+    );
+}
